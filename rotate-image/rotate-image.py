bfa71e0cb3b33dc90b112a22ed817f065d2502a3
Sumit Sahu <Sumitsahu896@users.noreply.github.com>
2021-05-23 17:03:20 -0400

    Time: 36 ms (56.90%), Space: 14.1 MB (83.07%) - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -3,11 +3,36 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix[0])
-        for i in range(n // 2 + n % 2): # To find the mid point to which we have to run
-            for j in range(n // 2):
-                temp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - i - 1][n - j - 1]
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
-                matrix[j][n - i - 1] = matrix[i][j]
-                matrix[i][j] = temp
+        # n = len(matrix[0])
+        # for i in range(n // 2 + n % 2): # To find the mid point to which we have to run
+        #     for j in range(n // 2):
+        #         temp = matrix[n - 1 - j][i]
+        #         matrix[n - 1 - j][i] = matrix[n - i - 1][n - j - 1]
+        #         matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+        #         matrix[j][n - i - 1] = matrix[i][j]
+        #         matrix[i][j] = temp
+        
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+        return matrix
+        
+        
+#         self.transpose(matrix)
+#         self.reflect(matrix)
+        
+#     def transpose(self, matrix):
+#             n = len(matrix)
+#             for i in range(n):
+#                 for j in range(i , n):
+#                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                    
+#     def reflect(self, matrix):
+#             n = len(matrix)
+#             for i in range(n):
+#                 for j in range(n // 2 + n % 2):
+#                     matrix[i][j], matrix[i][-j - 1] = matrix[i][-j -1], matrix[i][j]
+                    
+                    
