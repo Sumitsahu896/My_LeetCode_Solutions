9d50e65be0a9f90c218245963f9529dcaa5633f0
Sumit Sahu <Sumitsahu896@users.noreply.github.com>
2021-05-24 19:18:38 -0400

    Time: 28 ms (77.10%), Space: 14.2 MB (72.09%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,12 +1,20 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        if (n == 1):
-            return 1
-        first = 1
-        second = 2
-        for i in range(3, n+1):
-            third = first + second
-            first = second
-            second = third
+#     def climbStairs(self, n: int) -> int:
+#         if (n == 1):
+#             return 1
+#         first = 1
+#         second = 2
+#         for i in range(3, n+1):
+#             third = first + second
+#             first = second
+#             second = third
         
-        return second
+#         return second
+    def __init__(self):
+            self.dict = {1:1, 2:2}
+    
+    
+    def climbStairs(self, n: int) -> int:    
+        if n not in self.dict:
+            self.dict[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dict[n]
