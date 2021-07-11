39d4ea46c71bf62df8fdc8e2a4dc03b179ef39f5
Sumit Sahu <Sumitsahu896@users.noreply.github.com>
2021-07-11 15:55:13 -0400

    Time: 728 ms (5.05%), Space: 28.4 MB (5.17%) - LeetHub

--- maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -10,3 +10,19 @@ class Solution:
         allCombinations = [frontSum[i] + backSum[k-i] for i in range(k+1)]
         
         return max(allCombinations)
+
+#         frontSum, backSum = 0, 0
+    
+#         for i in range(k):
+#             frontSum += cardPoints[i]
+            
+#         maxScore = frontSum
+        
+#         for i in range(k - 1,0,-1):
+#             backSum += cardPoints[len(cardPoints) - (k - i)]
+#             frontSum -= cardPoints[i]
+#             currentScore = backSum + frontSum
+#             maxScore = max(maxScore, currentScore)
+            
+            
+#         return maxScore
