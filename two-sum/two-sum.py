66010ac898fd4e397e98c50dc87ce53414f7b0ed
Sumit Sahu <Sumitsahu896@users.noreply.github.com>
2021-07-30 18:37:16 -0400

    Time: 60 ms (77.17%), Space: 15.5 MB (20.65%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -5,10 +5,18 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        h = {}
-        for i, num in enumerate(nums):
-            n = target - num
-            if n not in h:
-                h[num] = i
+        # h = {}
+        # for i, num in enumerate(nums):
+        #     n = target - num
+        #     if n not in h:
+        #         h[num] = i
+        #     else:
+        #         return [h[n], i]
+        
+        hash_map = {}
+        for index, num in enumerate(nums):
+            difference = target - num
+            if difference not in hash_map:
+                hash_map[num] = index
             else:
-                return [h[n], i]
+                return [hash_map[difference], index]
